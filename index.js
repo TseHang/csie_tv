d58ce2f0e097e8f3b1101870d86a030ea7b64f2d
TseHang <a54383813@gmail.com>
2016-05-31 13:48:47 +0800

    5/31

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ $.preload(images, 1, function(last){
 
 		//Preloading Out
     $('body').removeClass("loading");
-    var timeFlag = window.setInterval(animate , 1000); //animate Started
+    var timeFlag = window.setInterval(animate , 9000); //animate Started
 	}
   
   if (last){
