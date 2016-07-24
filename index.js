545df5222504d90e02655c125a8938bc08bddd7a
TseHang <a54383813@gmail.com>
2016-07-24 17:38:27 +0800

    update 7/24

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ var images = [
   'images/15.jpg',
   'images/16.jpg',
   'images/17.jpg',
-  'images/18.gif'
+  'images/18.gif',
+  'images/19.jpg'
 ];
 
 $.preload(images, 1, function(last){
@@ -36,7 +37,7 @@ $.preload(images, 1, function(last){
 
 		//Preloading Out
     $('body').removeClass("loading");
-    var timeFlag = window.setInterval(animate , 9000); //animate Started
+    var timeFlag = window.setInterval(animate , 1000); //animate Started
 	}
   
   if (last){
@@ -50,7 +51,7 @@ var count = 1 ;
 function animate() {
 	count++;
 	//console.log("111");
-	if ( count > 18 )
+	if ( count > 19 )
 		count = 1 ;
 
 	if (count == 18){
@@ -84,26 +85,24 @@ function animate() {
 			break;
 
 	}
-	//系女排
 	if (count == 1){
+		document.getElementById('content').children[0].innerHTML = "恭賀! 本系 盧文祥副教授！";
+		document.getElementById('content').children[1].innerHTML = "榮獲本校104學年度全校輔導傑出導師獎！！";
+	}
+	//系女排
+	if (count == 2){
 		document.getElementById('content').children[0].innerHTML = "資訊女排，熱血拿牌";
 		document.getElementById('content').children[1].innerHTML = "2016 春季成功盃冠軍 用生命在打排球";
 	}
-
 	//系男籃
-	if (count == 2){
+	if (count == 3){
 		document.getElementById('content').children[0].innerHTML = "資訊男籃，陽光宅男";
 		document.getElementById('content').children[1].innerHTML = "最帥，沒有之一";
 	}
 	//系棒
-	if (count == 3){
+	if (count == 4){
 		document.getElementById('content').children[0].innerHTML = "2015 南資盃 壘球團體賽 殿軍";
 		document.getElementById('content').children[1].innerHTML = "[系棒] 練球時間：每週三、五 15:00~18:00 自強球場";
-	}
-	//系排
-	if (count == 4){
-		document.getElementById('content').children[0].innerHTML = "2016 成大工院盃 男子排球賽 季軍";
-		document.getElementById('content').children[1].innerHTML = "[男排] 練球時間：每週二、五 18:00 光復球場";
 	}
 	//女籃
 	if (count == 5){
@@ -112,7 +111,7 @@ function animate() {
 	}
 	if (count == 6){
 		document.getElementById('content').children[0].innerHTML = "[賀] 本系男子排球隊勇獲105學年系際盃冠軍";
-		document.getElementById('content').children[1].innerHTML = "超強的啦！！";
+		document.getElementById('content').children[1].innerHTML = "[男排] 練球時間：每週二、五 18:00 光復球場";
 	}
 	//
 	if (count == 7){
@@ -159,7 +158,6 @@ function animate() {
 		document.getElementById('content').children[0].innerHTML = "2016 資訊系畢業典禮 丟帽儀式";
 		document.getElementById('content').children[1].innerHTML = "畢業，只是下一個旅程的開始";
 	}
-
 	//
 	if (count == 16){
 		document.getElementById('content').children[0].innerHTML = "別從別人的嘴裡，去認識你自己";
@@ -167,11 +165,15 @@ function animate() {
 	}
 	//
 	if (count == 17){
-		document.getElementById('content').children[0].innerHTML = "大家讀書加油！ 期末考加油！";
+		document.getElementById('content').children[0].innerHTML = "大家讀書加油！ 考試加油！";
 		document.getElementById('content').children[1].innerHTML = "圖片來源：毛叢叢紛絲專頁";
 	}
 	if (count == 18){
 		document.getElementById('content').children[0].innerHTML = "努力！我行的！";
 		document.getElementById('content').children[1].innerHTML = "GO!GO!";
+	}
+	if (count == 19){
+		document.getElementById('content').children[0].innerHTML = "恭賀！ 本系 陳培殷老師！";
+		document.getElementById('content').children[1].innerHTML = "指導研究團隊榮獲「第十六屆旺宏金矽獎半導體設計與應用大賽」應用組－銅獎!";
 	}
 }
