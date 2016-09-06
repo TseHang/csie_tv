7585f6d21ed1e7e929207f3d937115f9b6b1f8f2
TseHang <a54383813@gmail.com>
2016-09-07 01:47:19 +0800

    add video

--- index.js
+++ index.js
@@ -29,15 +29,21 @@ var images = [
   'images/19.jpg'
 ];
 
+// 計算總共要讀取幾個
+var count = inputs.length ;
+var tmpCounter = 0 ;
+var timeFlag; // 控制換圖片的時間
+var timeInterval = 9000 ;
+var video = document.getElementById("video");
+
 $.preload(images, 1, function(last){
 
-	if ($(this)[0] == 'images/10.jpg'){
+	if ($(this)[0] == inputs[Math.round(count/2)].name ){
 		console.log('start!!');
-		console.log('開始動畫囉!!');
 
 		//Preloading Out
     $('body').removeClass("loading");
-    var timeFlag = window.setInterval(animate , 9000); //animate Started
+    timeFlag = window.setTimeout(next , timeInterval); //animate Started
 	}
   
   if (last){
@@ -45,31 +51,28 @@ $.preload(images, 1, function(last){
   }
 });
 
-//Preloading Out
-var count = 1 ;
+function next() {
+	// console.log(tmpCounter);
 
-function animate() {
-	count++;
-	//console.log("111");
-	if ( count > 19 )
-		count = 1 ;
+	// 判斷到底需不需要從頭讀
+	tmpCounter ++;
+	tmpCounter = (tmpCounter==count) ? 0 : tmpCounter ;
 
-	if (count == 18){
-		//'background-size':'cover',
-		$('#container').css({
-			'background-image':'url(\'images/'+count+'.gif\')',
-		})
+	// type: undefined --> Image
+	if(inputs[tmpCounter].type == undefined){
+		changeImage(tmpCounter);
+		changeFooter(tmpCounter);
+
+		//9秒後換下一個 
+		timeFlag = window.setTimeout(next , timeInterval);
 	}
-	else{
-		//'background-size':'cover',
-		$('#container').css({
-			'background-image':'url(\'images/'+count+'.jpg\')',
-		})
+	else if (inputs[tmpCounter].type == 'video'){
+
+		changeVideo(tmpCounter);
 	}
-	
-	//因為選舉 蓋到系會 會不清楚 所以調大小
-	
-	var fade_direction = count%4 ;
+
+	// 換動畫
+	fade_direction = tmpCounter%4 ;
 	switch (fade_direction) {
 		case 0 :
 			$('#content').animo({animation: "fadeInDown", duration: 0.5});
@@ -83,97 +86,31 @@ function animate() {
 		case 3 :
 			$('#content').animo({animation: "fadeInUp", duration: 0.5});
 			break;
-
-	}
-	if (count == 1){
-		document.getElementById('content').children[0].innerHTML = "恭賀! 本系 盧文祥副教授！";
-		document.getElementById('content').children[1].innerHTML = "榮獲本校104學年度全校輔導傑出導師獎！！";
-	}
-	//系女排
-	if (count == 2){
-		document.getElementById('content').children[0].innerHTML = "資訊女排，熱血拿牌";
-		document.getElementById('content').children[1].innerHTML = "2016 春季成功盃冠軍 用生命在打排球";
-	}
-	//系男籃
-	if (count == 3){
-		document.getElementById('content').children[0].innerHTML = "資訊男籃，陽光宅男";
-		document.getElementById('content').children[1].innerHTML = "最帥，沒有之一";
-	}
-	//系棒
-	if (count == 4){
-		document.getElementById('content').children[0].innerHTML = "2015 南資盃 壘球團體賽 殿軍";
-		document.getElementById('content').children[1].innerHTML = "[系棒] 練球時間：每週三、五 15:00~18:00 自強球場";
-	}
-	//女籃
-	if (count == 5){
-		document.getElementById('content').children[0].innerHTML = "2016 資訊女籃 成大工院盃 亞軍";
-		document.getElementById('content').children[1].innerHTML = "恭賀 兼具力與美的美女們奪得亞軍！";
-	}
-	if (count == 6){
-		document.getElementById('content').children[0].innerHTML = "[賀] 本系男子排球隊勇獲105學年系際盃冠軍";
-		document.getElementById('content').children[1].innerHTML = "[男排] 練球時間：每週二、五 18:00 光復球場";
-	}
-	//
-	if (count == 7){
-		document.getElementById('content').children[0].innerHTML = "[賀] 本系女子排球隊勇獲105學年系際盃季軍";
-		document.getElementById('content').children[1].innerHTML = "主任真幸福～";
-	}
-	//送舊
-	if (count == 8){
-		document.getElementById('content').children[0].innerHTML = "2016 送舊晚會 圓滿成功";
-		document.getElementById('content').children[1].innerHTML = "讓我們恭賀學長姐們順利畢業！！";
-	}
-	//小畢典
-	if (count == 9){
-		document.getElementById('content').children[0].innerHTML = "2016 送舊晚會 歌王";
-		document.getElementById('content').children[1].innerHTML = "神啊，救救我吧！";
-	}
-	//選舉系列
-	if (count == 10){
-		document.getElementById('content').children[0].innerHTML = "2016 送舊晚會";
-		document.getElementById('content').children[1].innerHTML = "感謝辛苦工人們～";
-	}
-	//會長選舉
-	if (count == 11){
-		document.getElementById('content').children[0].innerHTML = "2016 資訊系畢業典禮 ";
-		document.getElementById('content').children[1].innerHTML = "";
-	}
-	//
-	if (count == 12){
-		document.getElementById('content').children[0].innerHTML = "2016 資訊系畢業典禮 謝師禮";
-		document.getElementById('content').children[1].innerHTML = "";
-	}
-	//
-	if (count == 13){
-		document.getElementById('content').children[0].innerHTML = "2016 資訊系畢業典禮 撥穗";
-		document.getElementById('content').children[1].innerHTML = "";
-	}
-	//
-	if (count == 14){
-		document.getElementById('content').children[0].innerHTML = "2016 資訊系畢業典禮 ";
-		document.getElementById('content').children[1].innerHTML = "";
-	}
-	//
-	if (count == 15){
-		document.getElementById('content').children[0].innerHTML = "2016 資訊系畢業典禮 丟帽儀式";
-		document.getElementById('content').children[1].innerHTML = "畢業，只是下一個旅程的開始";
-	}
-	//
-	if (count == 16){
-		document.getElementById('content').children[0].innerHTML = "別從別人的嘴裡，去認識你自己";
-		document.getElementById('content').children[1].innerHTML = "圖片來源：毛叢叢 紛絲專頁";
-	}
-	//
-	if (count == 17){
-		document.getElementById('content').children[0].innerHTML = "大家讀書加油！ 考試加油！";
-		document.getElementById('content').children[1].innerHTML = "圖片來源：毛叢叢紛絲專頁";
-	}
-	if (count == 18){
-		document.getElementById('content').children[0].innerHTML = "努力！我行的！";
-		document.getElementById('content').children[1].innerHTML = "GO!GO!";
-	}
-	if (count == 19){
-		document.getElementById('content').children[0].innerHTML = "恭賀！ 本系 陳培殷老師！";
-		document.getElementById('content').children[1].innerHTML = "指導研究團隊榮獲「第十六屆旺宏金矽獎半導體設計與應用大賽」應用組－銅獎!";
 	}
 }
+
+function changeImage(counter){
+	$('#container').css({
+		'background-image':'url('+ inputs[counter].name +')',
+	})
+}
+
+function changeVideo(counter){
+	
+	$('#container').css("display" , "none");
+
+	// 播放影片，消掉圖片
+	video.src = inputs[counter].name ;
+	video.addEventListener("ended",function(){
+		console.log("影片播完囉～");
+
+		$('#container').css("display" , "block");
+		next();
+	});
+}
+
+function changeFooter(counter){
+		document.getElementById('content').children[0].innerHTML = inputs[counter].title;
+		document.getElementById('content').children[1].innerHTML = inputs[counter].subTitle;
+}
+
---
+++ input.js
@@ -0,0 +1,83 @@
+// input.js
+var inputs = [
+	{
+		name: 'images/1.jpg',
+		title: "恭賀! 本系 盧文祥副教授！" ,
+		subTitle: "榮獲本校104學年度全校輔導傑出導師獎！！"
+	},{
+		name: 'images/2.jpg',
+		title: "資訊女排，熱血拿牌" ,
+		subTitle: "2016 春季成功盃冠軍 用生命在打排球！！"
+	},{
+		name: 'images/3.jpg',
+		title: "資訊男籃，陽光宅男" ,
+		subTitle: "最帥，沒有之一"
+	},{
+		name: 'images/4.jpg',
+		title: "2015 南資盃 壘球團體賽 殿軍" ,
+		subTitle: "[系棒] 練球時間：每週三、五 15:00~18:00 自強球場"
+	},{
+		name: 'images/5.jpg',
+		title: "2016 資訊女籃 成大工院盃 亞軍" ,
+		subTitle: "恭賀 兼具力與美的美女們奪得亞軍！"
+	},{
+		name: 'images/6.jpg',
+		title: "[賀] 本系男子排球隊勇獲105學年系際盃冠軍" ,
+		subTitle: "[男排] 練球時間：每週二、五 18:00 光復球場"
+	},{
+		name: 'images/7.jpg',
+		title: "[賀] 本系女子排球隊勇獲105學年系際盃季軍" ,
+		subTitle: "主任真幸福～"
+	},{
+		name: 'images/8.jpg',
+		title: "2016 送舊晚會 圓滿成功" ,
+		subTitle: "讓我們恭賀學長姐們順利畢業！！"
+	},{
+		name: 'images/9.jpg',
+		title: "2016 送舊晚會 歌王" ,
+		subTitle: "神啊，救救我吧！"
+	},{
+		name: 'images/10.jpg',
+		title: "2016 送舊晚會" ,
+		subTitle: "感謝辛苦工人們～"
+	},{
+		name: 'images/11.jpg',
+		title: "2016 資訊系畢業典禮" ,
+		subTitle: ""
+	},{
+		name: 'images/12.jpg',
+		title: "2016 資訊系畢業典禮 謝師禮" ,
+		subTitle: ""
+	},{
+		name: 'images/13.jpg',
+		title: "2016 資訊系畢業典禮 撥穗" ,
+		subTitle: ""
+	},{
+		name: 'images/14.jpg',
+		title: "2016 資訊系畢業典禮" ,
+		subTitle: ""
+	},{
+		name: 'images/15.jpg',
+		title: "2016 資訊系畢業典禮 丟帽儀式" ,
+		subTitle: "畢業，只是下一個旅程的開始"
+	},{
+		name: 'images/16.jpg',
+		title: "別從別人的嘴裡，去認識你自己" ,
+		subTitle: "圖片來源：毛叢叢 紛絲專頁"
+	},{
+		name: 'images/17.jpg',
+		title: "大家讀書加油！ 考試加油！" ,
+		subTitle: "圖片來源：毛叢叢紛絲專頁"
+	},{
+		name: 'images/18.gif',
+		title: "努力！我行的！" ,
+		subTitle: "GO!GO!"
+	},{
+		name: 'images/19.jpg',
+		title: "恭賀！ 本系 陳培殷老師！" ,
+		subTitle: "指導研究團隊榮獲「第十六屆旺宏金矽獎半導體設計與應用大賽」應用組－銅獎!"
+	},{
+		type: 'video',
+		name: 'video/成大資工系.mp4'
+	}
+]
