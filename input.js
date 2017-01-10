d44137a4fd3a2392c5f91b36e675645e1f540b00
TseHang <a54383813@gmail.com>
2017-01-10 11:13:15 +0800

    update 106_presentation

--- input.js
+++ input.js
@@ -1,49 +1,93 @@
 // input.js
 var inputs = [
 	{
-		name: 'images/teacher.jpeg',
-		title: "恭賀! 本系 莊坤達 老師！" ,
-		subTitle: "榮獲 105學年度校優良教師！！"
+		name: 'images/106_presentation/1.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "連所長帥！"
 	},{
-		name: 'images/2.jpg',
-		title: "資訊女排，熱血拿牌" ,
-		subTitle: "2016 春季成功盃冠軍 用生命在打排球！！"
+		name: 'images/106_presentation/2.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "系主任帥！"
 	},{
-		name: 'images/3.jpg',
-		title: "資訊男籃，陽光宅男" ,
-		subTitle: "最帥，沒有之一"
+		name: 'images/106_presentation/3.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "~ wow ~"
 	},{
-		name: 'images/4.jpg',
-		title: "2015 南資盃 壘球團體賽 殿軍" ,
-		subTitle: "[系棒] 練球時間：每週三、五 15:00~18:00 自強球場"
+		name: 'images/106_presentation/4.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "爆猛籃球機"
 	},{
-		name: 'images/csie_girl_baseketball.jpg',
-		title: "2016 資訊女籃 系際盃 冠軍！" ,
-		subTitle: "恭賀 美女們奪得冠軍！！"
+		name: 'images/106_presentation/5.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "大三微算機專題，超猛der "
 	},{
-		name: 'images/mvp.jpg',
-		title: "2016 資訊女籃 全場MVP！" ,
-		subTitle: "超強的啦！！！！"
+		name: 'images/106_presentation/6.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "大三微算機專題，超猛der "
 	},{
-		name: 'images/6.jpg',
-		title: "[賀] 本系男子排球隊勇獲105學年系際盃冠軍" ,
-		subTitle: "[男排] 練球時間：每週二、五 18:00 光復球場"
+		name: 'images/106_presentation/7.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "大三微算機專題，超猛der "
 	},{
-		name: 'images/9.jpg',
-		title: "2016 送舊晚會 歌王" ,
-		subTitle: "神啊，救救我吧！"
+		name: 'images/106_presentation/8.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "大三微算機專題，超猛der "
 	},{
-		name: 'images/13.jpg',
-		title: "2016 資訊系畢業典禮 撥穗" ,
-		subTitle: ""
+		name: 'images/106_presentation/9.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "老師評分中... > <"
 	},{
-		name: 'images/14.jpg',
-		title: "2016 資訊系畢業典禮" ,
-		subTitle: ""
+		name: 'images/106_presentation/10.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "高中生們也來囉～"
 	},{
-		name: 'images/15.jpg',
-		title: "2016 資訊系畢業典禮 丟帽儀式" ,
-		subTitle: "畢業，只是下一個旅程的開始"
+		name: 'images/106_presentation/11.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "大三微算機專題，超猛der "
+	},{
+		name: 'images/106_presentation/12.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "緊張的presentation！"
+	},{
+		name: 'images/106_presentation/13.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "緊張的presentation！"
+	},{
+		name: 'images/106_presentation/14.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "哼！"
+	},{
+		name: 'images/106_presentation/15.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "感謝美麗姊姊工作人員"
+	},{
+		name: 'images/106_presentation/16.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "第一名"
+	},{
+		name: 'images/106_presentation/17.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "佳作"
+	},{
+		name: 'images/106_presentation/18.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "佳作"
+	},{
+		name: 'images/106_presentation/19.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "佳作"
+	},{
+		name: 'images/106_presentation/20.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "緊張！刺激！"
+	},{
+		name: 'images/106_presentation/21.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "黑白棋天才"
+	},{
+		name: 'images/106_presentation/22.jpg',
+		title: "106級 畢業專題展" ,
+		subTitle: "帥"
 	},{
 		name: 'images/16.jpg',
 		title: "別從別人的嘴裡，去認識你自己" ,
@@ -60,8 +104,71 @@ var inputs = [
 		name: 'images/18.gif',
 		title: "努力！我行的！衝一波吧！" ,
 		subTitle: "GO!GO!"
-	},{
-		type: 'video',
-		source: document.getElementById("video_csie")
 	}
 ]
+// var inputs = [
+// 	{
+// 		name: 'images/teacher.jpeg',
+// 		title: "恭賀! 本系 莊坤達 老師！" ,
+// 		subTitle: "榮獲 105學年度校優良教師！！"
+// 	},{
+// 		name: 'images/2.jpg',
+// 		title: "資訊女排，熱血拿牌" ,
+// 		subTitle: "2016 春季成功盃冠軍 用生命在打排球！！"
+// 	},{
+// 		name: 'images/3.jpg',
+// 		title: "資訊男籃，陽光宅男" ,
+// 		subTitle: "最帥，沒有之一"
+// 	},{
+// 		name: 'images/4.jpg',
+// 		title: "2015 南資盃 壘球團體賽 殿軍" ,
+// 		subTitle: "[系棒] 練球時間：每週三、五 15:00~18:00 自強球場"
+// 	},{
+// 		name: 'images/csie_girl_baseketball.jpg',
+// 		title: "2016 資訊女籃 系際盃 冠軍！" ,
+// 		subTitle: "恭賀 美女們奪得冠軍！！"
+// 	},{
+// 		name: 'images/mvp.jpg',
+// 		title: "2016 資訊女籃 全場MVP！" ,
+// 		subTitle: "超強的啦！！！！"
+// 	},{
+// 		name: 'images/6.jpg',
+// 		title: "[賀] 本系男子排球隊勇獲105學年系際盃冠軍" ,
+// 		subTitle: "[男排] 練球時間：每週二、五 18:00 光復球場"
+// 	},{
+// 		name: 'images/9.jpg',
+// 		title: "2016 送舊晚會 歌王" ,
+// 		subTitle: "神啊，救救我吧！"
+// 	},{
+// 		name: 'images/13.jpg',
+// 		title: "2016 資訊系畢業典禮 撥穗" ,
+// 		subTitle: ""
+// 	},{
+// 		name: 'images/14.jpg',
+// 		title: "2016 資訊系畢業典禮" ,
+// 		subTitle: ""
+// 	},{
+// 		name: 'images/15.jpg',
+// 		title: "2016 資訊系畢業典禮 丟帽儀式" ,
+// 		subTitle: "畢業，只是下一個旅程的開始"
+// 	},{
+// 		name: 'images/16.jpg',
+// 		title: "別從別人的嘴裡，去認識你自己" ,
+// 		subTitle: "圖片來源：毛叢叢 紛絲專頁"
+// 	},{
+// 		name: 'images/miss.jpeg',
+// 		title: "只有在嚐過之後，才能在回憶時甜美" ,
+// 		subTitle: "圖片來源：毛叢叢 紛絲專頁"
+// 	},{
+// 		name: 'images/textAvy.jpeg',
+// 		title: "" ,
+// 		subTitle: "文字來源：故事貿易公司"
+// 	},{
+// 		name: 'images/18.gif',
+// 		title: "努力！我行的！衝一波吧！" ,
+// 		subTitle: "GO!GO!"
+// 	},{
+// 		type: 'video',
+// 		source: document.getElementById("video_csie")
+// 	}
+// ]
